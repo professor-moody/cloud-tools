7cfa22231793afbb7ffc822106a34bd30e250c62
professor-moody <professor.moody@pm.me>
2026-01-24 16:34:45 -0600

    afe7c916790af446c4c470b3d527009e4b8b0440

--- bin/Prowler/ui/app/(prowler)/attack-paths/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-/**
- * Landing page for Attack Paths feature
- * Redirects to the integrated attack path analysis view
- */
-export default function AttackPathsPage() {
-  redirect("/attack-paths/query-builder");
-}
