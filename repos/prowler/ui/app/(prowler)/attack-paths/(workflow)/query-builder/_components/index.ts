38a486ccf8fc227db207166ab44e2146b381f470
professor-moody <professor.moody@pm.me>
2026-02-07 16:34:59 -0600

    afe7c916790af446c4c470b3d527009e4b8b0440

--- repos/prowler/ui/app/(prowler)/attack-paths/(workflow)/query-builder/_components/index.ts
+++
@@ -1,7 +0,0 @@
-export { ExecuteButton } from "./execute-button";
-export * from "./graph";
-export * from "./node-detail";
-export { QueryParametersForm } from "./query-parameters-form";
-export { QuerySelector } from "./query-selector";
-export { ScanListTable } from "./scan-list-table";
-export { ScanStatusBadge } from "./scan-status-badge";
