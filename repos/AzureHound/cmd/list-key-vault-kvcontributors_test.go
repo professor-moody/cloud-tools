c08a2c8647e56e17a14c0599b070f5282dfa0fcf
professor-moody <professor.moody@pm.me>
2026-04-25 16:39:06 -0500

    BloodHoundAD/AzureHound@1e211df74bd5b425babb60d9b7ab60fdc1581a60

---
+++ repos/AzureHound/cmd/list-key-vault-kvcontributors_test.go
@@ -0,0 +1,102 @@
+// Copyright (C) 2022 Specter Ops, Inc.
+//
+// This file is part of AzureHound.
+//
+// AzureHound is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AzureHound is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"context"
+	"testing"
+
+	"github.com/bloodhoundad/azurehound/v2/client/mocks"
+	"github.com/bloodhoundad/azurehound/v2/constants"
+	"github.com/bloodhoundad/azurehound/v2/enums"
+	"github.com/bloodhoundad/azurehound/v2/models"
+	"github.com/bloodhoundad/azurehound/v2/models/azure"
+	"go.uber.org/mock/gomock"
+)
+
+func init() {
+	setupLogger()
+}
+
+func TestListKeyVaultKVContributors(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ctx := context.Background()
+
+	mockClient := mocks.NewMockAzureClient(ctrl)
+
+	mockRoleAssignmentsChannel := make(chan azureWrapper[models.KeyVaultRoleAssignments])
+	mockTenant := azure.Tenant{}
+	mockClient.EXPECT().TenantInfo().Return(mockTenant).AnyTimes()
+	channel := listKeyVaultKVContributors(ctx, mockRoleAssignmentsChannel)
+
+	go func() {
+		defer close(mockRoleAssignmentsChannel)
+
+		mockRoleAssignmentsChannel <- NewAzureWrapper(
+			enums.KindAZKeyVaultRoleAssignment,
+			models.KeyVaultRoleAssignments{
+				KeyVaultId: "foo",
+				RoleAssignments: []models.KeyVaultRoleAssignment{
+					{
+						RoleAssignment: azure.RoleAssignment{
+							Name: "matching-assignment",
+							Properties: azure.RoleAssignmentPropertiesWithScope{
+								RoleDefinitionId: constants.KeyVaultContributorRoleID,
+							},
+						},
+					},
+					{
+						RoleAssignment: azure.RoleAssignment{
+							Name: "non-matching-assignment",
+							Properties: azure.RoleAssignmentPropertiesWithScope{
+								RoleDefinitionId: constants.OwnerRoleID,
+							},
+						},
+					},
+				},
+			},
+		)
+	}()
+
+	result, ok := <-channel
+	if !ok {
+		t.Fatalf("failed to receive from channel")
+	}
+
+	wrapper, ok := result.(azureWrapper[models.KeyVaultKVContributors])
+	if !ok {
+		t.Fatalf("unexpected type in channel: %T", result)
+	}
+
+	if wrapper.Data.KeyVaultId != "foo" {
+		t.Errorf("expected KeyVaultId 'foo', got '%s'", wrapper.Data.KeyVaultId)
+	}
+
+	if len(wrapper.Data.KVContributors) != 1 {
+		t.Fatalf("expected 1 kv contributor, got %d", len(wrapper.Data.KVContributors))
+	}
+
+	if wrapper.Data.KVContributors[0].KVContributor.Name != "matching-assignment" {
+		t.Errorf("expected kv contributor name 'matching-assignment', got '%s'", wrapper.Data.KVContributors[0].KVContributor.Name)
+	}
+
+	if _, ok := <-channel; ok {
+		t.Error("should not have received from channel")
+	}
+}
