dd4306bab5df2e03d6e97f02af55a4a01b625bbd
professor-moody <professor.moody@pm.me>
2026-06-06 16:37:50 -0500

    BloodHoundAD/AzureHound@195e96f7ae0f625c9986c6ec7fa1a88e4593f1db

---
+++ repos/AzureHound/models/app-fic.go
@@ -0,0 +1,59 @@
+// Copyright (C) 2022 Specter Ops, Inc.
+//
+// This file is part of AzureHound.
+//
+// AzureHound is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AzureHound is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package models
+
+import (
+	"encoding/json"
+)
+
+type AppFIC struct {
+	FIC   json.RawMessage `json:"fic"`
+	AppId string          `json:"appId"`
+}
+
+func (s *AppFIC) MarshalJSON() ([]byte, error) {
+	output := make(map[string]any)
+	output["appId"] = s.AppId
+
+	if s.FIC == nil {
+		return nil, nil
+	}
+
+	if fic, err := OmitEmpty(s.FIC); err != nil {
+		return nil, err
+	} else {
+		output["fic"] = fic
+		return json.Marshal(output)
+	}
+}
+
+type AppFICs struct {
+	FICs       []AppFIC `json:"fics"`
+	AppId      string   `json:"appId"`
+	TenantId   string   `json:"tenantId"`
+	TenantName string   `json:"tenantName"`
+}
+
+type FICData struct {
+	Audiences   []string `json:"audiences"`
+	ID          string   `json:"id"`
+	Issuer      string   `json:"issuer"`
+	Name        string   `json:"name"`
+	Subject     string   `json:"subject"`
+	Description string   `json:"description"`
+}
